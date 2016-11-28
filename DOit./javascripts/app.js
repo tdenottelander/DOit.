72d52fb30123c0229c59406a21395ca0169c5c72
Tom den Ottelander <tdenottelander@gmail.com>
2016-11-28 12:34:26 +0100

    ToDo input toegevoegd

--- DOit./javascripts/app.js
+++ DOit./javascripts/app.js
@@ -1,25 +1,50 @@
 var main = function () {
     "use strict";
     
-    var addCommentFromInputBox = function(){
-        var $new_comment;
-        if ($(".comment-input input").val() !== ""){
-            $new_comment = $("<p>").text($(".comment-input input").val()).hide();
-            $(".comments").append($new_comment);
-            $new_comment.fadeIn();
-            $(".comment-input input").val("");
+    var addToDoItem = function(){
+        var $new_Div;
+        var $new_ToDo;
+        var $new_Checkbox;
+        var $new_Date;
+        var $new_AlarmDate;
+        var $new_AlarmTime;
+        var $new_DeleteButton;
+        var $new_importance
+        if ($(".todo-input input").val() !== ""){
+            //We still need to check if alarm is before date of ToDo.
+            $new_Div = $("<div>").addClass("List");
+            $new_Checkbox = $("<input type = checkbox>");
+            $new_importance = $(".todo-input select").val() + " | ";
+            $new_Date = " " + $(".todo-input input[title=dateInput]").val() + " | ";
+            $new_ToDo = $(".todo-input input[title=textInput]").val() + " | ";
+            $new_AlarmDate = $(".todo-input input[title=alarmInputDate]").val() + " ";
+            $new_AlarmTime = $(".todo-input input[title=alarmInputTime]").val() + " | ";
+            $new_DeleteButton = $("<img/>").attr('src', 'Images/icon_delete.png').attr('alt', 'Delete icon');
+            
+            $new_Div.append($new_Checkbox, $new_importance, $new_Date, $new_ToDo, $new_AlarmDate, $new_AlarmTime, $new_DeleteButton);
+            $(".List1").append($new_Div);
+            $(".todo-input input").val("");
         }
     }
     
-    $(".comment-input button").on("click", function(event) {
-        addCommentFromInputBox();
+    $(".todo-input button").on("click", function(event) {
+        addToDoItem();
     });
     
-    $(".comment-input input").on("keypress", function (event) { 
+    $(".todo-input input").on("keypress", function (event) { 
         if(event.keyCode === 13){
-            addCommentFromInputBox();
+            addToDoItem();
         }
     });
+    
+    var addList = function(){
+        var $new_Div;
+        var $new_List;
+        
+        $new_Div = $(HTMLDivElement()).addClass("List");
+        
+    
+    }
     
     /*
     // create and hide our content as a div
@@ -37,13 +62,15 @@ var main = function () {
     
     var addToDoFromInputBox = function(){
         var $new_comment
-        if($(".addtask text") !== ""){
-            $new_comment = $("<p>").text($(".addtask text").value()).hide();
-            $(".List").append($new_comment);
-        
+        if($(".addToDo input") !== ""){
+            $new_comment = $("<p>").text($(".addToDo text").value()).hide();
+            $(".List1").append($new_comment);
         }
-    
     }
+    
+    $(".addtask button").on("click", function(event) {
+        addToDoFromInputBox();
+    });
     
 }
 
